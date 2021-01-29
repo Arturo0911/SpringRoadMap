c6faaa2a5b4d5c691915cba48bc194c9842df1e2
Arturon <anegreiross@outlook.com>
2021-01-29 16:37:09 -0500

    created new route

--- service-server/demo/src/main/java/com/example/service/demo/DemoApplication.java
+++ service-server/demo/src/main/java/com/example/service/demo/DemoApplication.java
@@ -2,12 +2,20 @@ package com.example.service.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue= "Arturon") String name){
+		return String.format("Hello %s", name);
 	}
 
 }
