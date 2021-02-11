94147985c45fee9bc53302977a36c445f42c7f5e
Arturo Negreiros <anegreiross@outlook.com>
2021-02-10 23:21:40 -0500

    watching series with my love

--- service-server/demo/src/main/java/com/example/service/demo/res/RestProductController.java
+++ service-server/demo/src/main/java/com/example/service/demo/res/RestProductController.java
@@ -1,8 +1,7 @@
 package com.example.service.demo.res;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
