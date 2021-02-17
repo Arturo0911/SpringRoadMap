6483cce9784f02dd148b55a3c5c8fda7e38c8b20
Arturo Negreiros <anegreiross@outlook.com>
2021-02-17 15:54:58 -0500

    added new JAR spring project

---
+++ SpringServiceDemo/src/main/java/com/service/demo/SpringServiceDemo/SpringServiceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.service.demo.SpringServiceDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServiceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringServiceDemoApplication.class, args);
+	}
+
+}
---
+++ SpringServiceDemo/src/test/java/com/service/demo/SpringServiceDemo/SpringServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.demo.SpringServiceDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServiceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
