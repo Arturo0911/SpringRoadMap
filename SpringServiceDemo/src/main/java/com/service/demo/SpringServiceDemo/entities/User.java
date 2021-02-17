2abb59b79107aee92d98cf9a3b7f09573c2e3492
Arturo Negreiros <anegreiross@outlook.com>
2021-02-17 16:24:36 -0500

    imported java persistence in maven xml

---
+++ SpringServiceDemo/src/main/java/com/service/demo/SpringServiceDemo/entities/User.java
@@ -0,0 +1,41 @@
+package com.service.demo.SpringServiceDemo.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor @NoArgsConstructor
+public class User {
+
+
+    @Id
+    @Column(name ="EMAIL")
+    @Getter @Setter
+    @NotNull @NotBlank @NotEmpty
+    private String email;
+
+    @Column(name = "NAME")
+    @Getter @Setter
+    @NotNull @NotBlank @NotEmpty
+    private String name;
+
+    @Column(name = "PASSWORD")
+    @Getter @Setter
+    @NotNull @NotBlank @NotEmpty
+    private String password;
+
+
+}
