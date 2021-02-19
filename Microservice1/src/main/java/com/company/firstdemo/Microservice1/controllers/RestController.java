35d2f188db0d542efd55b457c9e0156b86a1c09d
Arturo Negreiros <anegreiross@outlook.com>
2021-02-19 17:22:47 -0500

    implementing ResponseStatus

--- Microservice1/src/main/java/com/company/firstdemo/Microservice1/controllers/RestController.java
+++ Microservice1/src/main/java/com/company/firstdemo/Microservice1/controllers/RestController.java
@@ -2,11 +2,19 @@ package com.company.firstdemo.Microservice1.controllers;
 
 
 import com.company.firstdemo.Microservice1.entities.ToDo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.method.annotation.JsonViewResponseBodyAdvice;
 
 import javax.validation.Valid;
+
+/**
+ * @author Arturo Negreiros Samanez -> Arturo0911
+ *
+ * */
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
@@ -21,6 +29,10 @@ public class RestController {
         return "Description is: "+toDo.getDescription();
     }
 
+
+    /*
+    * using binding result, we catch the errors and show such a String.
+    * */
     @RequestMapping("/doto")
     public String getSecondToDo(@Valid ToDo toDo, BindingResult result){
 
@@ -29,4 +41,10 @@ public class RestController {
         }
         return "Description is: "+toDo.getDescription();
     }
+    @RequestMapping("/response")
+    public ResponseEntity<JsonResponseBody>returnData(){
+        return ResponseEntity.status(HttpStatus.OK).header("jwt", jwt)
+    }
+
+
 }
