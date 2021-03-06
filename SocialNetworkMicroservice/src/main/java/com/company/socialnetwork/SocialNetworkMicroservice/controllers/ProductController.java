9774971f4e41a5a2a2996976f13c67801fe6605c
Arturo Negreiros <anegreiross@outlook.com>
2021-03-06 01:38:31 -0500

    ok

--- SocialNetworkMicroservice/src/main/java/com/company/socialnetwork/SocialNetworkMicroservice/controllers/ProductController.java
+++ SocialNetworkMicroservice/src/main/java/com/company/socialnetwork/SocialNetworkMicroservice/controllers/ProductController.java
@@ -5,7 +5,6 @@ import com.company.socialnetwork.SocialNetworkMicroservice.entities.User;
 import com.company.socialnetwork.SocialNetworkMicroservice.services.UserService;
 import com.company.socialnetwork.SocialNetworkMicroservice.services.errorhandlers.UserNotInDataBaseException;
 import com.company.socialnetwork.SocialNetworkMicroservice.utilities.JsonResponseBody;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
