84d791fd0b8c8104356c600432c6a8ad4ea26029
Arturo0911 <anegreiross@outlook.com>
2021-03-12 23:35:07 -0500

    added userFormater

---
+++ SocialNetworkMicroservice/src/main/java/com/company/socialnetwork/SocialNetworkMicroservice/utilities/structures/UserStructure.java
@@ -0,0 +1,44 @@
+package com.company.socialnetwork.SocialNetworkMicroservice.utilities.structures;
+
+
+import com.company.socialnetwork.SocialNetworkMicroservice.entities.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @author Arturo Negreiros
+ */
+
+@AllArgsConstructor @NoArgsConstructor
+public class UserStructure {
+
+    /**
+     * TODO:
+     *      - the goal or purpose of this class is build a object with the necessary parameters
+     *          to send to the client.
+     *      - An User object with the follow parameters:
+     *          -- userId
+     *          -- userName
+     *          -- userLastName
+     *          -- userBirth
+     *          -- userPhoneNumber
+     */
+    @Getter @Setter
+    public HashMap<Object, Object> userManagement;
+
+    public HashMap formatUser(User user){
+        return null;
+    }
+
+    public List formatUsers(List<User> users){
+        return null;
+    }
+
+
+
+}
