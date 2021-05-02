79ee99860500fbd6f5b9cd68eb182a7e9a1f7a97
arturo <anegreiross@outlook.com>
2021-05-01 20:09:27 -0500

    ok

--- demo/src/main/java/com/example/demo/demo/daos/EmployeeDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.demo.daos;
-
-
-import com.example.demo.demo.entities.Employee;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeDao extends CrudRepository<Employee, Integer> {}
--- demo/src/main/java/com/example/demo/demo/daos/ToDoDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.demo.daos;
-
-
-import com.example.demo.demo.entities.ToDo;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ToDoDao extends CrudRepository<ToDo, Integer> {}
